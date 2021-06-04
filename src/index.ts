c4f04442338ef201fa91d31dc01aef7ec3b783e5
김경식 <envy7937@gmail.com>
2021-06-04 16:53:46 +0900

    Use Interface and Class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
-// parmeter 뒤의 ?는 해당 parameter가 optional 하다는 걸 알려준다.
-const sayHi = (name: string, age: number, gender?: string): string => {
-	return (`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+	name: string
+	age: number
+	gender: string
 }
 
-console.log(sayHi("envy", 27, "male"));
+const person = {
+	name: "Kyoungsik",
+	age: 27,
+	gender: "male"
+}
 
-export { };
+class Human2 {
+	public name: string
+	public age: number
+	public gender: string
+	
+	constructor(name: string, age: number, gender: string) {
+		this.name = name
+		this.age = age
+		this.gender = gender
+	}
+}
+
+const envy = new Human2("Envy", 26, "male")
+
+const sayHi = (person: Human): string => {
+	return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
+}
+
+console.log(sayHi(person));
+console.log(sayHi(envy));
+
+export { }
